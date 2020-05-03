79e469d18503b82cf0f46c5cc8e843840882d613
jmfiaschi <jm.fiaschi@gmail.com>
2020-05-03 20:10:04 +0200

    restrict the pattern name of the varaible : {{ [a-zA-Z0-9_-]+ }}

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,10 @@ pub trait EnvApply {
 /// Apply environment variables on a string.
 impl EnvApply for Vars {
     fn apply(text: String) -> String {
-        let key_pattern_regex = Regex::new("[{]{2}[ ]*([^{ ]+)[ ]*[}]{2}").unwrap();
+        let key_pattern_regex = Regex::new("[{]{2}[ ]*([a-zA-Z0-9_-]+)[ ]*[}]{2}").unwrap();
         let mut result = text.clone();
         for key_pattern in key_pattern_regex.find_iter(text.as_ref()) {
-            let key_regex = Regex::new("[{]{2}[ ]*([^{ ]+)[ ]*[}]{2}").unwrap();
+            let key_regex = Regex::new("[{]{2}[ ]*([a-zA-Z0-9_-]+)[ ]*[}]{2}").unwrap();
             let key = key_regex.replace(key_pattern.as_str(), "$1").to_string();
             let value = match var(key.as_str()) {
                 Ok(value) => value,
@@ -30,10 +30,10 @@ impl EnvApply for Vars {
 /// Apply OS environment variables on a string.
 impl EnvApply for VarsOs {
     fn apply(text: String) -> String {
-        let key_pattern_regex = Regex::new("[{]{2}[ ]*([^{ ]+)[ ]*[}]{2}").unwrap();
+        let key_pattern_regex = Regex::new("[{]{2}[ ]*([a-zA-Z0-9_-]+)[ ]*[}]{2}").unwrap();
         let mut result = text.clone();
         for key_pattern in key_pattern_regex.find_iter(text.as_ref()) {
-            let key_regex = Regex::new("[{]{2}[ ]*([^{ ]+)[ ]*[}]{2}").unwrap();
+            let key_regex = Regex::new("[{]{2}[ ]*([a-zA-Z0-9_-]+)[ ]*[}]{2}").unwrap();
             let key = key_regex.replace(key_pattern.as_str(), "$1").to_string();
             let value = match var_os(key.as_str()) {
                 Some(os) => os.to_string_lossy().to_string(),
@@ -49,18 +49,21 @@ impl EnvApply for VarsOs {
 #[cfg(test)]
 mod env_applier_test {
     use super::*;
+
     #[test]
     fn it_should_apply_env_var_on_string() {
-        let string = r#"{"test":"{{ ENV_KEY }}"}"#;
-        set_var("ENV_KEY", "ENV_VALUE");
+        let string = r#"{"field1":"{{ ENV_KEY1 }}","field2":"{{ENV_KEY2}}"}"#;
+        set_var("ENV_KEY1", "ENV_VALUE1");
+        set_var("ENV_KEY2", "ENV_VALUE2");
         let result = Vars::apply(string.to_string());
-        assert_eq!(r#"{"test":"ENV_VALUE"}"#, result);
+        assert_eq!(r#"{"field1":"ENV_VALUE1","field2":"ENV_VALUE2"}"#, result);
     }
     #[test]
     fn it_should_apply_env_var_os_on_string() {
-        let string = r#"{"test":"{{ HOME }}"}"#;
-        set_var("HOME", "MY_HOME");
+        let string = r#"{"field1":"{{ ENV_KEY1 }}","field2":"{{ENV_KEY2}}"}"#;
+        set_var("ENV_KEY1", "ENV_VALUE1");
+        set_var("ENV_KEY2", "ENV_VALUE2");
         let result = VarsOs::apply(string.to_string());
-        assert_eq!(r#"{"test":"MY_HOME"}"#, result);
+        assert_eq!(r#"{"field1":"ENV_VALUE1","field2":"ENV_VALUE2"}"#, result);
     }
 }
