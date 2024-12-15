8ee60193640e31021aa23cd06c40a17a24859227
github-action-benchmark <github@users.noreply.github.com>
2024-12-15 22:34:10 +0000

    add Benchmark (cargo) benchmark result for bb02ea7c5b241836935c2fa84d5f14cbc4cc3678

---
+++ docs/bench/master/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1734302050281,
+  "repoUrl": "https://github.com/jmfiaschi/env_applier",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jm.fiaschi@gmail.com",
+            "name": "jm.fiaschi",
+            "username": "jmfiaschi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bb02ea7c5b241836935c2fa84d5f14cbc4cc3678",
+          "message": "fix(project): remove mandatory separator in the argument of apply_wit… (#3)\n\n* fix(project): remove mandatory separator in the argument of apply_with_prefix",
+          "timestamp": "2024-12-15T23:23:11+01:00",
+          "tree_id": "7ee140f2193775e72fe43c5fb0151cb3ef54ccc6",
+          "url": "https://github.com/jmfiaschi/env_applier/commit/bb02ea7c5b241836935c2fa84d5f14cbc4cc3678"
+        },
+        "date": 1734302049987,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "String/Bench apply",
+            "value": 556,
+            "range": "± 36",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
