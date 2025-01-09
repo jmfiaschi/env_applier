6d2d6aacf1554adf1f8ad9efa3f76e3386c66ade
github-action-benchmark <github@users.noreply.github.com>
2025-01-09 19:19:30 +0000

    add Benchmark (cargo) benchmark result for 08578c8b9811184b6610dcadda8870df148804a4

---
+++ docs/bench/main/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1736450370946,
+  "repoUrl": "https://github.com/jmfiaschi/env_applier",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jm.fiaschi@gmail.com",
+            "name": "Jean-Marc FIASCHI",
+            "username": "jmfiaschi"
+          },
+          "committer": {
+            "email": "jm.fiaschi@gmail.com",
+            "name": "Jean-Marc FIASCHI",
+            "username": "jmfiaschi"
+          },
+          "distinct": true,
+          "id": "08578c8b9811184b6610dcadda8870df148804a4",
+          "message": "chore(project): switch to main branch",
+          "timestamp": "2025-01-09T20:15:57+01:00",
+          "tree_id": "bb0bae8bf1da6bea506bf8689389d750de3cab5f",
+          "url": "https://github.com/jmfiaschi/env_applier/commit/08578c8b9811184b6610dcadda8870df148804a4"
+        },
+        "date": 1736450370680,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "String/Bench apply",
+            "value": 564,
+            "range": "± 10",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
